f9d456128547ae65bbffb4dbc893b71a45985fe8
nitin <nitinjam8800@gmail.com>
2023-06-02 20:08:12 +0530

    day-2

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 import express from "express";
 import { config } from "dotenv";
 import userRouter from "./routes/user.js"
+import taskRouter from "./routes/task.js"
 import cookieParser from "cookie-parser";
+import { errorMiddleWare } from "./middlewares/error.js";
 
-
-export const app=express();
+export const app = express();
 
 config({
-    path:"./data/config.env",
+    path: "./data/config.env",
 })
 
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
 
-
 //using routes
-app.use("/api/v1/users",userRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
+
+//using error middleware
+app.use(errorMiddleWare)
+
 
 
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 import { User } from "../models/user.js";
 import bcrypt from "bcrypt";
 import { setCookie } from "../utils/cookie.js";
+import ErrorHandler from "../middlewares/error.js";
 
 
 export const getAllUsers = async (req, res) => {
@@ -17,12 +18,9 @@ export const RegisterNewUser = async (req, res) => {
     try {
         const { name, email, password } = req.body;
         let user = await User.findOne({ email });
-        if (user) {
-            return res.status(404).json({
-                status: false,
-                message: "User Already exist"
-            })
-        }
+
+        if (user) return next(new ErrorHandler("User Already exist", 404))
+
         const hasedPassword = await bcrypt.hash(password, 10)
         user = await User.create({
             name,
@@ -31,10 +29,7 @@ export const RegisterNewUser = async (req, res) => {
         });
         setCookie(user, res, 201, "User Registerd Succussfully") // to set cookie
     } catch (error) {
-        res.status(404).json({
-            status: false,
-            error,
-        });
+       next(error)
     }
 };
 
@@ -45,12 +40,7 @@ export const LoginUser = async (req, res) => {
         const { email, password } = req.body;
         const user = await User.findOne({ email }).select("+password");
 
-        if (!user) {
-            return res.status(404).json({
-                status: false,
-                message: "Invalid User or Password"
-            })
-        }
+        if (!user) return next(new ErrorHandler("Invalid User or Password", 404))
 
         const isMatch = await bcrypt.compare(password, user.password);
 
@@ -63,10 +53,7 @@ export const LoginUser = async (req, res) => {
 
         setCookie(user, res, 200, `Welcome back ${user.name}`)
     } catch (error) {
-        return res.status(404).json({
-            status: false,
-            error,
-        })
+        next(error)
     }
 }
 
